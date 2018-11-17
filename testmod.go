d20b0c531890bf04a5e1432df729114d4684e36f
puppy <puppy@dog.lan>
2018-11-17 12:35:44 +0300

    Added language support to Hi()

--- testmod.go
+++ testmod.go
@@ -3,6 +3,13 @@ package testmod
 import "fmt"
 
 // Hi returns a friendly greeting.
-func Hi(name string) string {
-	return fmt.Sprintf("Hello dear %s!", name)
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hello, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
